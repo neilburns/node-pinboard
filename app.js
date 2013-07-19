101a99f724c61b3c43841e971a7168da85510401
neilburns <neillburns@gmail.com>
2013-07-19 22:51:04 +0100

    final

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-    // app.js
-     
-    // A.1
-    var express = require('express'),
-        app = express();
-        server = require('http').createServer(app),
-        io = require('socket.io').listen(server);
-     
-    // A.2
-    app.configure(function() {
-        app.use(express.static(__dirname + '/public'));
-    });
-     
-    // A.3
-    server.listen(1337);
+var express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
+
+app.configure(function() {
+    app.use(express.static(__dirname + '/public'));
+});
+
+io.sockets.on('connection', function(socket) {
+	socket.on('createNote', function(data) {
+		socket.broadcast.emit('onNoteCreated', data);
+	});
+
+	socket.on('updateNote', function(data) {
+		socket.broadcast.emit('onNoteUpdated', data);
+	});
+
+	socket.on('moveNote', function(data){
+		socket.broadcast.emit('onNoteMoved', data);
+	});
+
+	socket.on('deleteNote', function(data){
+		socket.broadcast.emit('onNoteDeleted', data);
+	});
+});
+
+server.listen(1337);
